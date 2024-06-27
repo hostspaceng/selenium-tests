3bf97389e3bd00c49345c30fda68294ef8a72ceb
brightballo <brightenxxx@gmail.com>
2024-06-27 20:58:48 +0100

    push

--- headless.py
+++ headless.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,7 +7,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-import logging
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,28 +17,22 @@ email = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
 url = os.getenv("URL")
 
-
 options = Options()
-
-# options.add_argument('--headless=new')
+options.add_argument('--headless')
 options.add_argument('--window-size=1920,1080')  # Adjust as necessary
-options.add_argument('--enable-logging')
-options.add_argument('--v=1')
-
 options.add_argument('--disable-gpu')  # GPU is not used in headless mode
 options.add_argument('--no-sandbox')  # Bypass OS security model, necessary for Docker
 options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource problems
 options.add_argument('--remote-debugging-port=9222')  # Remote debugging
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36")
+options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36')
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-# driver = webdriver.Chrome()
 driver.get(url)
 
 try:
-    WebDriverWait(driver, 10).until(EC.title_is("Sign in | Helium Health"))
+    WebDriverWait(driver, 150).until(EC.title_is("Sign in | Helium Health"))
     logging.info("Login page loaded successfully.")
+    driver.save_screenshot("login_page.png")  # Screenshot of the login page
     
     email_input = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.NAME, "emailOrPhone"))
@@ -54,6 +48,7 @@ try:
     
     WebDriverWait(driver, 10).until(EC.title_contains("Patients | Helium Health"))
     logging.info("Login successful - Redirected to patients.")
+    driver.save_screenshot("patients_page.png")  # Screenshot after successful login
 except Exception as e:
     logging.error("Login Unsuccessful - {}".format(str(e)))
 finally:
